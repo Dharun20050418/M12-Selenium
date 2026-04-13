40975a265dc8bf228bcb455f9f5423ec67ba5df6
dharu <dharu@Dharun>
2026-04-13 09:56:49 +0530

    This is a my 4th Commit

--- src/test/java/Demo/Paractice_2.java
+++ src/test/java/Demo/Paractice_2.java
@@ -3,8 +3,11 @@ package Demo;
 public class Paractice_2 {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Irfaahhhhh...........");
-		
+		int a=1;
+		int b=5;
+		if(a==b) {
+			a++;
+		}
+		System.out.println("Csk Got 6th Cup");
 	}
 }
