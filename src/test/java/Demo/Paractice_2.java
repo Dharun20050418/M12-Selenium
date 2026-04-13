5db56a25cf21e05fadf9f809ffb448f2bb50d769
dharu <dharu@Dharun>
2026-04-13 10:35:00 +0530

    I change now only

--- src/test/java/Demo/Paractice_2.java
+++ src/test/java/Demo/Paractice_2.java
@@ -2,10 +2,12 @@ package Demo;
 
 public class Paractice_2 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		int a=1;
 		int CSK=5;
-		if(a!=CSK) {
+		if(a!=CSK) 
+		{
 			a++;
 		}
 		System.out.println("CSK Got 6th Cup");
