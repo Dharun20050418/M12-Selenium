f7f763fd060ff7fb9e8d51fd98e5ba5d1cfd85a7
dharu <dharu@Dharun>
2026-04-13 10:23:26 +0530

    from ATE

--- src/test/java/Demo/Paractice_2.java
+++ src/test/java/Demo/Paractice_2.java
@@ -3,11 +3,8 @@ package Demo;
 public class Paractice_2 {
 
 	public static void main(String[] args) {
-		int a=1;
-		int CSK=5;
-		if(a!=CSK) {
-			a++;
-		}
-		System.out.println("CSK Got 6th Cup");
+		System.out.println("Hey There......!");
+		System.out.println("Hello Everyone");
+		System.out.println("Hello Everyone1");
 	}
 }
